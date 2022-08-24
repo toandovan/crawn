9eb7458dd9c73b19e66f1a555aa0dabcda8d2ff9
ANSI <toan.do.khmtk17@hcmut.edu.vn>
2022-08-24 09:32:37 +0700

    update

--- crawn.py
+++ crawn.py
@@ -2,7 +2,7 @@ import requests
 import json
 import os
 import multiprocessing
-from multiprocessing import Pool
+from multiprocessing import Pool, freeze_support
 import shutil
 f = open('data.json')
 data = json.load(f)
@@ -11,7 +11,7 @@ def zoom(layer, URL, data):
         os.mkdir("./"+str(layer['name'])+"/"+str(data['z']))
     except FileExistsError:
         pass
-    pool = multiprocessing.Pool(64)
+    pool = multiprocessing.Pool(8)
     processes=[pool.apply_async(x_function, args=(layer, URL, data, x)) for x in range(data['xmin'],data['xmax'])]
     result = [p.get() for p in processes]
 
@@ -28,12 +28,13 @@ def y_function(layer, URL, data, x, y):
     response = requests.get(URL_format)
     if len(response.content)!=0:
         open("./"+str(layer['name'])+"/"+str(data['z'])+"/"+str(x)+"/"+str(y)+".png", "wb").write(response.content)
-
-for layer in data['layer']:
-    URL = layer['URL']
-    try:
-        os.mkdir(str(layer['name']))
-    except FileExistsError:
-        pass
-    for i in data['data']:
-        multiprocessing.Process(target=zoom, args=(layer,URL,i)).start()
+if __name__ == '__main__':
+    freeze_support()
+    for layer in data['layer']:
+        URL = layer['URL']
+        try:
+            os.mkdir(str(layer['name']))
+        except FileExistsError:
+            pass
+        for i in data['data']:
+            multiprocessing.Process(target=zoom, args=(layer,URL,i)).start()
